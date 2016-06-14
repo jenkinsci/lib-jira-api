b0dae43e833fa3a557a2631314c31956cb5de5ad
Kohsuke Kawaguchi <kk@kohsuke.org>
2016-06-14 17:32:41 +0900

    Added another convenience connector method
    ... that encourages the use of common credential file.

--- src/main/java/org/jenkinsci/jira/JIRA.java
+++ src/main/java/org/jenkinsci/jira/JIRA.java
@@ -3,9 +3,11 @@ package org.jenkinsci.jira;
 import com.atlassian.jira.rest.client.api.JiraRestClient;
 import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.Properties;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -17,6 +19,14 @@ public class JIRA {
     public static JiraRestClient connect(URL jiraUrl, String username, String password) throws IOException {
         return new AsynchronousJiraRestClientFactory()
                 .createWithBasicHttpAuthentication(URI.create(jiraUrl.toExternalForm()), username, password);
+    }
 
+    public static JiraRestClient connect(URL jiraUrl) throws IOException {
+        Properties props = new Properties();
+        try (FileReader r = new FileReader(System.getProperty("user.home")+".jenkins-ci.org")) {
+            props.load(r);
+        }
+
+        return connect(new URL("https://issues.jenkins-ci.org/"), props.getProperty("userName"), props.getProperty("password"));
     }
 }
